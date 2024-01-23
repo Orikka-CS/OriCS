a20d0f007b8f9092b77319f16ec34408f4cd3e31
slyni <slyni@naver.com>
2024-01-23 09:48:10 +0900

    트리아드나 효과 수정

--- script/c124121039.lua
+++ script/c124121039.lua
@@ -3,11 +3,13 @@ local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTargetRange(POS_DEFENSE,0)
 	e1:SetCondition(s.con1)
+	e1:SetTarget(s.tar1)
 	e1:SetOperation(s.op1)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
@@ -43,29 +45,33 @@ end
 function s.nfil1(c,e,tp)
 	return c:IsCode(87979586) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.con1(e,c,og)
+function s.con1(e,c)
 	if c==nil then
 		return true
 	end
 	local tp=c:GetControler()
-	return Duel.GetFlagEffect(tp,id)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_HAND,0,1,c)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
 		and Duel.IsExistingMatchingCard(s.nfil1,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_HAND,0,1,c)
 end
-function s.op1(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+function s.tar1(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local tg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,0,1,c)
-	if #tg==0 then
-		return
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.nfil1,tp,LOCATION_DECK+LOCATION_GRAVE,0,0,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,0,1,c)
 	if #g>0 then
-		Duel.SendtoGrave(tg,REASON_COST+REASON_DISCARD)
-		sg:AddCard(c)
-		sg:Merge(g)
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	else
+		return false
 	end
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+	g:DeleteGroup()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectMatchingCard(tp,s.nfil1,tp,LOCATION_DECK+LOCATION_GRAVE,0,0,1,nil,e,tp)
+	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_DEFENSE)
 end
 function s.tar3(e,c)
 	return c:IsCode(87979586)
