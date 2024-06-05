fd996245244f8e66e68b6644141c7d3aae8f0dc8
slyni <slyni@naver.com>
2024-06-06 04:32:48 +0900

    셰터드 섀도우

---
+++ script/c124121060.lua
@@ -0,0 +1,118 @@
+--셰터드 섀도우 스테라
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,nil,2,2,s.pfun1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.con1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.tar1)
+	e1:SetOperation(s.op1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.con2)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.tar2)
+	e2:SetOperation(s.op2)
+	c:RegisterEffect(e2)
+end
+function s.pfil1(c,lc,sumtype,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK,lc,sumtype,tp) and c:IsRace(RACE_WINGEDBEAST,lc,sumtype,tp)
+end
+function s.pfun1(g,lc,sumtype,tp)
+	return g:IsExists(s.pfil1,1,nil,lc,sumtype,tp)
+end
+function s.con1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.cfil1(c)
+	return c:IsAbleToGraveAsCost() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WINGEDBEAST) and c:IsLevel(4)
+end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfil1,tp,LOCATION_DECK,0,nil)
+	if chk==0 then
+		return #g>0
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local sg=g:Select(tp,1,1,nil)
+	Duel.SendtoGrave(sg,REASON_COST)
+end
+function s.tfil1(c)
+	return c:IsAbleToHand() and c:IsCode(24094653)
+end
+function s.tar1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.tfil1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.tfil1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.nfil2(c,tp,rp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp) and
+		c:GetPreviousTypeOnField()&TYPE_FUSION~=0 and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsPreviousSetCard(0xfa2) and (c:IsReason(REASON_BATTLE) or rp==1-tp)
+end
+function s.con2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not eg:IsContains(c) and eg:IsExists(s.nfil2,1,nil,tp,rp)
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
+end
+function s.tfil2(c)
+	return (c:IsCode(24094653) or c:IsCode(48130397)) and c:CheckActivateEffect(true,true,false)~=nil
+		and c:IsAbleToGraveAsCost()
+end
+function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		if e:GetLabel()==0 then return false end
+		e:SetLabel(0)
+		return c:IsAbleToExtraAsCost()
+			and Duel.IsExistingMatchingCard(s.tfil2,tp,LOCATION_DECK,0,1,nil)
+	end
+	e:SetLabel(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tfil2,tp,LOCATION_DECK,0,1,1,nil)
+	local te,ceg,cep,cev,cre,cr,crp=g:GetFirst():CheckActivateEffect(false,true,true)
+	Duel.SendtoDeck(c,nil,2,REASON_COST)
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetProperty(te:GetProperty())
+	local tg=te:GetTarget()
+	if tg then
+		tg(e,tp,ceg,cep,cev,cre,cr,crp,1)
+	end
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+	Duel.ClearOperationInfo(0)
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then
+		return
+	end
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then
+		op(e,tp,eg,ep,ev,re,r,rp)
+	end
+end
