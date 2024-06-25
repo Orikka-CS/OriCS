e80650bee359a778aee085a003f0437150835aa0
id187 <limajaka123@naver.com>
2024-06-25 13:01:34 +0900

    효과 수정

--- script/c124161024.lua
+++ script/c124161024.lua
@@ -16,10 +16,11 @@ function s.initial_effect(c)
 	--effect 2
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(0,LOCATION_MZONE)
-	e2:SetValue(s.val2)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(e,c) return c~=e:GetHandler() and c:IsSetCard(0xf21) end)
+	e2:SetValue(function(e,te) return te:GetOwnerPlayer()==1-e:GetHandlerPlayer() and te:IsActivated() and te:GetHandler():IsSpell() end)
 	c:RegisterEffect(e2)
 end
 
@@ -55,9 +56,4 @@ function s.op1(e,tc,tp,sg,chk)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetLabel(e:GetLabel())
 	c:RegisterEffect(e1)
-end
-
---effect2
-function s.val2(e,c)
-	return c:IsFaceup() and c:IsSetCard(0xf21) and c~=e:GetHandler()
 end
