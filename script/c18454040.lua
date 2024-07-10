aa7ab25165621f73bf14a60fe743f5e7ea9dfe20
slyni <slyni@naver.com>
2024-07-11 02:57:02 +0900

    Update c18454040.lua

--- script/c18454040.lua
+++ script/c18454040.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--tohand
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
