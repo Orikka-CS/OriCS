01179f76c1c99d36dd4ff5c41912cc248e8e03c7
id187 <limajaka123@naver.com>
2024-09-15 00:48:30 +0900

    주석 추가

--- script/c124161096.lua
+++ script/c124161096.lua
@@ -11,6 +11,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.tg1)
 	e1:SetOperation(s.op1)
 	c:RegisterEffect(e1)
+	--effect 2
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
