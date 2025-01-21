59d2c320f36011f62eb1744a5fb1f4e911fd1a4f
slyni <slyni@naver.com>
2025-01-21 20:12:28 +0900

    여우귀

---
+++ script/c47450011.lua
@@ -0,0 +1,144 @@
+--집념과 복수의 화신
+local m=47450011
+local cm=_G["c"..m]
+
+function cm.initial_effect(c)
+	
+	--module summon
+	c:EnableReviveLimit()
+	aux.AddModuleProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_MODULE),nil,2,10,nil)
+
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCondition(cm.con1)
+	e0:SetOperation(cm.op1)
+	c:RegisterEffect(e0)
+
+	--equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetTarget(cm.eqtg)
+	e1:SetOperation(cm.eqop)
+	c:RegisterEffect(e1)
+
+	--equip2
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(cm.eqtg2)
+	e2:SetOperation(cm.eqop2)
+	c:RegisterEffect(e2)
+
+	--destroy_all
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetTarget(cm.destg)
+	e3:SetOperation(cm.desop)
+	c:RegisterEffect(e3)
+
+end
+
+function cm.con1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return bit.band(c:GetSummonType(),SUMMON_TYPE_MODULE)==SUMMON_TYPE_MODULE
+end
+function cm.op1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTarget(cm.tar11)
+	Duel.RegisterEffect(e1,tp)
+end
+function cm.tar11(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsCode(m) and bit.band(sumtype,SUMMON_TYPE_MODULE)==SUMMON_TYPE_MODULE
+end
+
+function cm.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_GRAVE)
+end
+
+function cm.eqop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+
+	local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,TYPE_MONSTER)
+	local tc=g:GetFirst()
+
+	if not Duel.Equip(tp,tc,c) then return end
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetValue(cm.eqlimit)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e1)
+end
+
+function cm.eqlimit(e,c)
+  return e:GetOwner()==c
+end
+
+function cm.tgfilter(c)
+	local atk=e:GetHandler():GetAttack()
+	return c:IsType(TYPE_MONSTER) and c:IsAttackBelow(atk)
+end
+
+function cm.eqtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(cm.tgfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_MZONE)
+end
+
+function cm.eqop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+
+	local g=Duel.SelectMatchingCard(tp,cm.tgfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	local tc=g:GetFirst()
+
+	if not Duel.Equip(tp,tc,c) then return end
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetValue(cm.eqlimit)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e1)
+end
+
+function cm.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local atk=e:GetHandler():GetOriginalAttack()
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAttackBelow,tp,0,LOCATION_MZONE,1,nil,atk) end
+	local g=Duel.GetMatchingGroup(Card.IsAttackBelow,tp,0,LOCATION_MZONE,nil,atk)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0,nil)
+end
+
+function cm.desop(e,tp,eg,ep,ev,re,r,rp)
+	local atk=e:GetHandler():GetOriginalAttack()
+	local g=Duel.GetMatchingGroup(Card.IsAttackBelow,tp,0,LOCATION_MZONE,nil,atk)
+	Duel.Destroy(g,REASON_EFFECT)
+end
