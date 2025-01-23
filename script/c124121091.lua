f2bfd0476fafec3021214767113bfb4159b61544
slyni <slyni@naver.com>
2025-01-23 19:33:30 +0900

    흑의룡

---
+++ script/c124121091.lua
@@ -0,0 +1,90 @@
+--흑의룡의 스테인 스케일
+local s,id=GetID()
+function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(LOCATION_PZONE|LOCATION_EMZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK))
+	e1:SetValue(s.val1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_REMOVE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(s.tar2)
+	c:RegisterEffect(e1)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_DRAGON))
+	e3:SetValue(300)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCategory(CATEGORY_TOGRAVE|CATEGORY_SPECIAL_SUMMON)
+	e4:SetCondition(s.con4)
+	e4:SetCost(s.cost4)
+	e4:SetTarget(s.tar4)
+	e4:SetOperation(s.op4)
+	c:RegisterEffect(e4)
+end
+function s.val1(e,re,rp)
+	return re:IsMonsterEffect()
+end
+function s.tar2(e,c,tp,r)
+	return r==REASON_EFFECT and c:IsControler(tp) and c:IsLocation(LOCATION_PZONE|LOCATION_EMZONE) and c:IsFaceup()
+		and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.con4(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	return rc:IsType(TYPE_LINK) and re:IsMonsterEffect()
+end
+function s.cost4(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return c:IsAbleToHandAsCost()
+	end
+	local fid=c:GetFieldID()
+	e:SetLabel(fid)
+	Duel.SendtoHand(c,nil,REASON_COST)
+	c:RegisterFlagEffect(id,RESET_CHAIN|RESET_EVENT|RESETS_STANDARD,0,0,fid)
+end
+function s.tfil4(c)
+	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_LINK) and c:IsAbleToGrave()
+end
+function s.tar4(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.tfil4,tp,LOCATION_EXTRA,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.op4(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tfil4,tp,LOCATION_EXTRA,0,1,1,nil)
+	local fid=e:GetLabel()
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and c:GetFlagEffectLabel(id)==fid then
+		local b1=Duel.CheckPendulumZones(tp)
+		local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		if not b1 and not b2 then
+			return
+		end
+		if b1 and (not b2 or not c:IsCanBeSpecialSummoned(e,0,tp,false,false) or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
+			Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+		else
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
---
+++ script/c124121092.lua
@@ -0,0 +1,94 @@
+--흑의룡의 스피어드 스케일
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.pfil1,1,1)
+	c:SetSPSummonOnce(id)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY|EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCategory(CATEGORY_TOHAND|CATEGORY_SPECIAL_SUMMON)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.tar1)
+	e1:SetOperation(s.op1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCondition(s.con2)
+	e2:SetTarget(s.tar2)
+	e2:SetOperation(s.op2)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function s.pfil1(c,scard,sumtype,tp)
+	return c:IsLevelBelow(4) and (c:IsRace(RACE_DRAGON,scard,sumtype,tp) or c:IsType(TYPE_NORMAL,scard,sumtype,tp))
+end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return c:IsReleasable()
+	end
+	Duel.Release(c,REASON_COST)
+end
+function s.tfil1(c,e,tp,ec)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_NORMAL) and c:IsType(TYPE_PENDULUM)
+		and (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and ((c:IsLocation(LOCATION_DECK) and Duel.GetMZoneCount(tp,ec)>0)
+				or (c:IsLocation(LOCATION_EXTRA) and c:IsFaceup() and Duel.GetLocationCountFromEx(tp,tp,ec,c)>0)
+			)))
+end
+function s.tar1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.tfil1,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil,e,tp,c)
+	end
+	Duel.SetPossibleOperation(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+	Duel.SetPossibleOperation(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.tfil1,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil,e,tp,nil)
+	local tc=g:GetFirst()
+	aux.ToHandOrElse(tc,tp,
+		function()
+			return (tc:IsLocation(LOCATION_DECK) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0)
+				or (tc:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,nil,tc)>0)
+		end,
+		function()
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end,
+		aux.Stringid(id,0)
+	)
+end
+function s.con2(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp
+end
+function s.tfil2(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_LINK) and c:IsLink(1)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+end
+function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return c:IsAbleToExtra() and Duel.IsExistingMatchingCard(s.tfil2,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SendtoDeck(c,nil,2,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.tfil2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
