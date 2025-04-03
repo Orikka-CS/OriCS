49ed7efc0ed0916b48b7593b099d8dbf0f7df27b
slyni <slyni@naver.com>
2025-04-03 15:35:18 +0900

    Update c124121096.lua

--- script/c124121096.lua
+++ script/c124121096.lua
@@ -93,7 +93,7 @@ function s.tfil2(c,e,tp)
 end
 function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 			and Duel.IsExistingMatchingCard(s.tfil2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
