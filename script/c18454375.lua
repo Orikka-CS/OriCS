76d05ee8a26adbc213aee0de5ed6fefc80058dd8
slyni <slyni@naver.com>
2025-04-15 23:48:21 +0900

    미카 공주님을 석방하라!

---
+++ script/c18454375.lua
@@ -0,0 +1,105 @@
+--미카 공주님을 석방하라!
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=MakeEff(c,"A")
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
+	e1:SetCL(1,id,EFFECT_COUNT_CODE_OATH)
+	WriteEff(e1,1,"TO")
+	c:RegisterEffect(e1)
+	local e2=MakeEff(c,"I","G")
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCL(1,{id,1})
+	WriteEff(e2,2,"TO")
+	c:RegisterEffect(e2)
+end
+function s.tfil11(c)
+	return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsAbleToHand()
+end
+function s.tfil12(c)
+	return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_FIRE)
+end
+function s.tfil13(c,e,tp)
+	return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.tar1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		return chkc:IsLoc("G") and chkc:IsControler(tp) and s.tfil13(chkc,e,tp)
+	end
+	local b1=Duel.IEMCard(s.tfil11,tp,"D",0,1,nil)
+	local b2=Duel.IEMCard(s.tfil12,tp,"H",0,1,nil) and Duel.IsPlayerCanDraw(tp,2)
+	local b3=Duel.GetLocCount(tp,"M")>0 and Duel.IETarget(s.tfil13,tp,"G",0,1,nil,e,tp)
+	if chk==0 then
+		return b1 or b2 or b3
+	end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)},
+		{b3,aux.Stringid(id,2)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetProperty(0)
+		e:SetCategory(CATEGORY_SEARCH)
+		Duel.SOI(0,CATEGORY_TOHAND,nil,1,tp,"D")
+	elseif op==2 then
+		e:SetProperty(0)
+		e:SetCategory(CATEGORY_DRAW)
+		Duel.SOI(0,CATEGORY_DRAW,nil,0,tp,2)
+	elseif op==3 then
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.STaarget(tp,s.tfil13,tp,"G",0,1,1,nil,e,tp)
+		Duel.SOI(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	else
+		e:SetProperty(0)
+		e:SetCategory(0)
+	end
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SMCard(tp,s.tfil11,tp,"D",0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	elseif op==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+		local g=Duel.SMCard(tp,s.tfil12,tp,"H",0,1,1,nil)
+		if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)>0 then
+			Duel.Draw(tp,2,REASON_EFFECT)
+		end
+	elseif op==3 then
+		local tc=Duel.GetFirstTarget()
+		if tc:IsRelateToEffect(e) then
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function s.tfil2(c)
+	return c:IsFaceup() and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsAbleToHand()
+end
+function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then
+		return chkc:IsControler(tp) and chkc:IsOnField() and s.tfil2(chkc)
+	end
+	if chk==0 then
+		return Duel.IETarget(s.tfil2,tp,"O",0,1,nil) and c:IsAbleToHand()
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.STarget(tp,s.tfil2,tp,"O",0,1,1,nil)
+	g:AddCard(c)
+	Duel.SOI(0,CATEGORY_TOHAND,g,2,0,0)
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local g=Group.FromCards(c,tc):Filter(Card.IsRelateToEffect,nil,e)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
