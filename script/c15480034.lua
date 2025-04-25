d713e45bc7e9496edc4eed7dee1a8dbbd8ad211f
slyni <slyni@naver.com>
2025-04-25 09:51:37 +0900

    Update c15480034.lua

--- script/c15480034.lua
+++ script/c15480034.lua
@@ -119,9 +119,15 @@ function s.op2(e,tp,eg,ep,ev,re,r,rp)
 		ft4 = math.min(ect,ft4)
 	end
 	local loc=0
-	if ft1>0 then loc=loc+LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE end
-	if ft2>0 or ft3>0 or ft4>0 then loc=loc+LOCATION_EXTRA end
-	if loc==0 then return end
+	if ft1>0 then
+		loc=loc+LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE
+	end
+	if ft2>0 or ft3>0 or ft4>0 then
+		loc=loc+LOCATION_EXTRA
+	end
+	if loc==0 then
+		return
+	end
 	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.tfil2),tp,loc,0,nil,e,tp)
 	if #sg==0 then
 		return
