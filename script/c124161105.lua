23ae733ca77a97baab8228c645b4f136c3b20075
id187 <limajaka123@naver.com>
2025-09-12 10:16:24 +0900

    처리 수정

--- script/c124161105.lua
+++ script/c124161105.lua
@@ -85,9 +85,9 @@ function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tg2filter(chkc,e) end
 	local g=Duel.GetMatchingGroup(s.tg2filter,tp,LOCATION_MZONE,0,nil,e,tp)
 	if chk==0 then return #g>0 end
-	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_TARGET)
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_TARGET):GetFirst()
 	Duel.SetTargetCard(sg)
-	e:SetLabel(e:GetHandler():AnnounceAnotherRace(tp))
+	e:SetLabel(Duel.AnnounceRace(tp,1,RACE_ALL-sg:GetRace()))
 end
 
 function s.op2(e,tp,eg,ep,ev,re,r,rp)
