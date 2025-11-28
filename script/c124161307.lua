f898b2c87f0ff4b228685c8bfe39d0c0db9e368c
id187 <limajaka123@naver.com>
2025-11-28 23:19:48 +0900

    add effect

--- script/c124161307.lua
+++ script/c124161307.lua
@@ -8,6 +8,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e0)
 	--effect 1
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -19,14 +20,26 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--effect 2
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_SZONE,0)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(s.cst2)
 	e2:SetTarget(s.tg2)
-	e2:SetValue(aux.tgoval)
+	e2:SetOperation(s.op2)
 	c:RegisterEffect(e2)
+	--effect 3
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_SZONE,0)
+	e3:SetTarget(s.tg3)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
 end
 
 --effect 1
@@ -67,7 +80,31 @@ function s.op1(e,tp,eg,ep,ev,re,r,rp)
 end
 
 --effect 2
-function s.tg2(e,c)
+function s.cst2filter(c)
+	return c:IsFaceup() and c:IsAbleToHandAsCost()
+end
+
+function s.cst2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cst2filter,tp,LOCATION_ONFIELD,0,e:GetHandler())
+	if chk==0 then return #g>0 end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_RTOHAND)
+	Duel.SendtoHand(sg,nil,REASON_COST)
+end
+
+function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,124161293,0xf33,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_EARTH) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,124161293,0xf33,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_EARTH) then return end
+	local token=Duel.CreateToken(tp,124161293)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
+
+--effect 3
+function s.tg3(e,c)
 	if not (c:IsType(TYPE_EQUIP) and c:IsType(TYPE_SPELL) and (c:IsFaceup() or c:GetEquipTarget())) then return false end
 	local effs={c:GetOwnEffects()}
 	for _,eff in ipairs(effs) do
