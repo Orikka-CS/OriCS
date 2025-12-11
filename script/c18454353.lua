4eabefdbfcc42f545632dbae967a5c2d3f8405bf
slyni <47357272+slyni7@users.noreply.github.com>
2025-12-12 07:05:54 +0900

    Update c18454353.lua

--- script/c18454353.lua
+++ script/c18454353.lua
@@ -83,8 +83,13 @@ end
 function s.op3(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=re:GetHandler()
-	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
-		Duel.Destroy(eg,REASON_EFFECT)
+	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT)>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(3000)
+		c:RegisterEffect(e1)
 	end
 end
 function s.cost4(e,tp,eg,ep,ev,re,r,rp,chk)
