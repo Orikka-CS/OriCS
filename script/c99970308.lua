3cccc20f304cb698f173d6b7f5e10fe641c88793
YuL Mk.II <65627621+YuLMk2@users.noreply.github.com>
2026-01-06 09:33:56 -0500

    진리의 서
    Signed-off-by: YuL Mk.II <65627621+YuLMk2@users.noreply.github.com>

--- script/c99970308.lua
+++ script/c99970308.lua
@@ -47,7 +47,7 @@ function s.op1(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e1:SetValue(LOCATION_DECKTOP)
+		e1:SetValue(LOCATION_DECK)
 		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
 		sg:RegisterEffect(e1)
 	end
