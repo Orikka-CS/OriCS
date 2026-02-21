821be09bd0c692926f60e391f45ba2eb4c1d1c9b
WelrodMkII <32021870+WelrodMkII@users.noreply.github.com>
2026-02-21 18:24:00 +0900

    세츠나

---
+++ script/c99000268.lua
@@ -0,0 +1,99 @@
+--P:ID－「세츠나」
+local s,id=GetID()
+function s.initial_effect(c)
+	--order summon
+	aux.AddOrderProcedure(c,"L",nil,aux.FilterBoolFunctionEx(Card.IsFaceup),aux.FilterBoolFunctionEx(Card.IsAttackPos))
+	c:EnableReviveLimit()
+	--그 상대 몬스터를 공격력 500 올리는 장착 카드로 취급하여 이 카드에 장착한다(1장만 장착 가능).
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(s.eqcon)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
+	c:RegisterEffect(e1)
+	aux.AddEREquipLimit(c,s.eqcon,function(ec,_,tp) return ec:IsControler(1-tp) end,s.equipop,e1)
+	--상대의 패를 무작위로 1장 확인한다.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetHandler():GetEquipGroup():Filter(s.eqfilter,nil)
+	return #g==0
+end
+function s.eqfilter(c)
+	return c:GetFlagEffect(id)~=0
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToChangeControler() end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+end
+function s.equipop(c,e,tp,tc)
+	if not c:EquipByEffectAndLimitRegister(e,tp,tc,id) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	e1:SetValue(500)
+	tc:RegisterEffect(e1)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsMonster() and tc:IsControler(1-tp) and s.eqcon(e,tp,eg,ep,ev,re,r,rp) then
+		s.equipop(c,e,tp,tc)
+	end
+end
+function s.costfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EQUIP) and c:IsAbleToGraveAsCost()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_SZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g==0 then return end
+	local sc=g:RandomSelect(tp,1):GetFirst()
+	Duel.ConfirmCards(tp,sc)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local b1=sc:IsMonster()
+	local b2=b1 and sc:IsAbleToDeck()
+	local b3=b1 and ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	local op=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,2)},
+			{b2,aux.Stringid(id,3)},
+			{b3,aux.Stringid(id,4)})
+	if not op then return end
+	Duel.BreakEffect()
+	if op==2 then
+		Duel.SendtoDeck(sc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	elseif op==3 then
+		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+	end
+	Duel.ShuffleHand(1-tp)
+end
